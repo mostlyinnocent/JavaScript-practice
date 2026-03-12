78ddc45ef982f5c52ddcd45e7ef90f5944ea8650
mostlyinnocent <debdebraj453@gmail.com>
2026-03-12 20:11:41 +0530

    Add AQI display

--- WeatherApp/scripts/weather.js
+++ WeatherApp/scripts/weather.js
@@ -9,6 +9,16 @@ const forecastDays = document.querySelectorAll(".forecast-day");
 const forecastLables = document.querySelectorAll('.forecast-day span:first-child');
 const forecastTemps = document.querySelectorAll(".forecast-temp");
 const forecastIcons = document.querySelectorAll(".forecast-weather-icon");
+const aqiEl = document.querySelector('[data-aqi]');
+
+const AQI_LABELS = {
+    1: "Good",
+    2: "Moderate",
+    3: "Unhealthy (sensitive group)",
+    4: "Unhealthy",
+    5: "Very Unhealthy",
+    6: "Hazardous",
+};
 
 
 export function renderWeatherInfo(weatherData){
@@ -16,13 +26,18 @@ export function renderWeatherInfo(weatherData){
     const {temp_c, maxtemp_c ,temp_f, wind_kph, humidity, feelslike_c} = weatherData.current;
     const { condition: {icon} } = weatherData.current;
 
+    const { air_quality } = weatherData.current;
+    const aqi = air_quality["us-epa-index"];
+    const aqiText = AQI_LABELS[aqi];
+    aqiEl.textContent = aqiText;
+
     cityEl.textContent = name;
     dateTimeEl.textContent = localtime;
     tempEl.textContent = `${Math.round(temp_c)}°C`;
     windEl.textContent = `${wind_kph} kmph`;
     humidityEl.textContent = `${humidity}%`;
     weatherIconEl.src = "https:" + icon;
-    tempFeel.textContent = `${feelslike_c}°C`
+    tempFeel.textContent = `${feelslike_c}°C`;
 
     const forecast = weatherData.forecast.forecastday.slice(1);
 
@@ -37,7 +52,7 @@ export function renderWeatherInfo(weatherData){
         const forecastTemp = Math.round(day.day.maxtemp_c);
 
         const icon = "https:" + day.day.condition.icon;
-
+    
         forecastLables[i].textContent = weekday;
         forecastIcons[i].src = icon;
         forecastTemps[i].textContent = `${forecastTemp}°C`;
